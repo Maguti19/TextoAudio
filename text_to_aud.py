01f4ba5deeaff8e2b9ab867a3a98233ec1928e10
Maguti19 <143103741+Maguti19@users.noreply.github.com>
2023-08-24 11:02:30 -0500

    Create text_to_aud.py

---
+++ text_to_aud.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import os
+import time
+import glob
+import os
+from gtts import gTTS
+from PIL import Image
+
+st.title("Interfases Multimodales.")
+image = Image.open('text_to_audio.png')
+
+st.image(image, width=200)
